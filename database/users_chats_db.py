5f076971e7eac49979302231695bee81063a6890
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-09-18 11:55:19 +0530

    Refactor database settings by removing unused imports
    Removed IMDB and IMDB_TEMPLATE from imports and settings.

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,6 +1,6 @@
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
+from info import DATABASE_NAME, DATABASE_URI, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
 
 class Database:
     
@@ -114,11 +114,9 @@ class Database:
         default = {
             'button': SINGLE_BUTTON,
             'botpm': P_TTI_SHOW_OFF,
-            'file_secure': PROTECT_CONTENT,
-            'imdb': IMDB,
+            'file_secure': PROTECT_CONTENT,            
             'spell_check': SPELL_CHECK_REPLY,
-            'welcome': MELCOW_NEW_USERS,
-            'template': IMDB_TEMPLATE
+            'welcome': MELCOW_NEW_USERS            
         }
         chat = await self.grp.find_one({'id':int(id)})
         if chat:
